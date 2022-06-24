7bf598c06b33141f589dc9c1e2ee59172a10219b
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-25 07:33:52 +0800

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -29,9 +29,6 @@ const defaultMenu = {
 ║Database: %rtotalreg dari %totalreg
 ║Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
 
-THANKS TO:
-• ArullOfc
-• All Creator Bot
 %readmore`.trimStart(), 
   header: '┌─「 %category 」',
   body: '├ %cmd %islimit %isPremium',
