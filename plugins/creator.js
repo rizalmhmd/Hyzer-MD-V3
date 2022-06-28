5d2724bb5b468dc518f6f6edf3b9a4f8fd51e579
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-28 13:43:58 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
+conn.sendContact(m.chat, 6285822347348, ArullOfc, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
