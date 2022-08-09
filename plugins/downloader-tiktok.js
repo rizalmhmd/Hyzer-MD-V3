37de8e255527e62905d78760c95a14588cc1d150
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-09 20:59:09 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -5,7 +5,7 @@ if (!args[0]) throw 'Uhm..url nya mana?'
 let res = await fetch(`https://api.xteam.xyz/dl/tiktok?url=${args[0]}&APIKEY=NezukoTachibana281207`)
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendButtonVid(m.chat, json.result.link_dl1, 'Nih Om', @zall, 'Thanks', `Thanks`, m)
+    await conn.sendButtonVid(m.chat, json.result.link_dl1, 'Nih Om', watermark, 'Thanks', `Thanks`, m)
 }
 handler.command = /^tiktok$/i
 handler.tags = ['downloader']
