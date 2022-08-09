a190a056afd92f6cba46b248d1af6072330c5d2e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-09 08:25:47 +0700

    Update fun-bkphub.js

--- plugins/fun-bkphub.js
+++ plugins/fun-bkphub.js
@@ -1,9 +1,7 @@
 let fetch = require('node-fetch')
-  if (command == 'caribokep') {
-if (!text) throw `Contoh penggunaan ${usedPrefix}${command} japan`
-try {
+let handler = async (m, { text }) => {
+    if (!text) throw 'Cari apa?'
 let f = await fetch(`https://api.lolhuman.xyz/api/xnxxsearch?apikey=azzbot&query=${text}`)
-let xx = await f.json()
 let str = xx.result.map((v, index) => {
         return `${1 + index}. Judul *${v.title}*
 views: ${v.views}
@@ -11,9 +9,6 @@ duration: ${v.duration}
 uploader: ${v.uploader}
 link: ${v.link}`.trim()
     }).join('\n\n')
-    await conn.sendButton(m.chat, str, wm, null, [
-                ['Dlbokep!', `${usedPrefix}dlbokep ${xx.result[0].link}`]
-            ], m, fdoc)
 }
 
 handler.help = ['caribokep']
