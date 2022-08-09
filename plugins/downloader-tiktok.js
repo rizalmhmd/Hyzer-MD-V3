ce7b982192bfa04f34f551ff1c16901b7a0ed086
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-09 20:28:47 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,11 +1,15 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'url nya mana ngenn?'
+if (!args[0]) throw 'Uhm..url nya mana?'
 let res = await fetch(`https://api.xteam.xyz/dl/tiktok?url=${args[0]}&APIKEY=NezukoTachibana281207`)
- if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let json = await res.json()
-let caption = { caption, name, thumb_img, link_dl1, }
-    await conn.sendButtonVid(m.chat, json.result.link_dl1, caption, '@ZALLxBOTZ', `Thanks`, m)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { caption, name, thumb_img, link_dl1, } = json.result
+await conn.sendFile(m.chat, link_dl1, 'tt.mp4', `
+𝙉𝙖𝙢𝙖 : *${name}*
+𝘾𝙖𝙥𝙩𝙞𝙤𝙣 : *${caption}*
+`.trim(), m)
 }
 
 handler.help = ['tiktok <url>']
