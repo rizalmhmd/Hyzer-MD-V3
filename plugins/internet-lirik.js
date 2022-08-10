189514436e07ca43d734ce8263f6a33faa8a48f9
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-10 13:10:55 +0700

    Update internet-lirik.js

--- plugins/internet-lirik.js
+++ plugins/internet-lirik.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} dandelions`
-  let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
+  let res = await fetch('https://zenzapis.xyz/searching/kazelyrics?query=${text}&apikey=f9fccfcff1'))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.status) throw json
