14790eeb1699510443ecf984b8454f5c6f099c69
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-19 22:53:35 +0700

    Update anime-cosplay.js

--- plugins/anime-cosplay.js
+++ plugins/anime-cosplay.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://rest-beni.herokuapp.com/api/randomimage/cosplay', 'Nihh Om', wm, m)
+  conn.sendFile(m.chat, 'https://zenzapis.xyz/randomimage/cosplay?apikey=f9fccfcff1', 'Nihh Om', wm, m)
 }
 handler.help = ['cosplay']
 handler.tags = ['anime']
