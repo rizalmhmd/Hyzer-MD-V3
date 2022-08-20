c199a4f0e69812d6166ac33081e1b2852d046da1
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-20 14:57:09 +0700

    Update asupan-korea.js

--- plugins/asupan-korea.js
+++ plugins/asupan-korea.js
@@ -1,6 +1,7 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/korea'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.korea', m)
+let korea = 'https://api.zacros.my.id/asupan/korea'
+    conn.sendButtonImg(m.chat, korea, 'Nih', wm2, 'NEXT', '.korea', m)
 }
 handler.help = ['korea']
 handler.tags = ['asupan']
