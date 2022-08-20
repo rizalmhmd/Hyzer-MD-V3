c58df983eacc20b3bae0a9e63b8ea151018c1e1e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-20 22:25:11 +0700

    Update anime-cosplay.js

--- plugins/anime-cosplay.js
+++ plugins/anime-cosplay.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-let nyenye = `https://zenzapis.xyz/randomimage/cosplay?apikey=f9fccfcff1`
-  conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.cosplay`, m) 
+let cosplay = `https://api.zacros.my.id/randomimg/cosplay`
+  conn.sendButtonImg(m.chat, cosplay, 'Nih', wm2, 'Next', `.cosplay`, m) 
 }
 handler.help = ['cosplay']
 handler.tags = ['anime']
