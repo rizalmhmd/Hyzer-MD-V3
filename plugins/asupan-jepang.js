478c4f14ef1369db2aa4ac0627892f45b2d0161f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-20 14:54:41 +0700

    Update asupan-jepang.js

--- plugins/asupan-jepang.js
+++ plugins/asupan-jepang.js
@@ -1,6 +1,7 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/japan'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.jepang', m)
+let jepang = 'https://api.zacros.my.id/asupan/japan'
+    conn.sendButtonImg(m.chat, jepang, 'Nih', wm2, 'NEXT', '.jepang', m)
 }
 handler.help = ['jepang']
 handler.tags = ['asupan']
