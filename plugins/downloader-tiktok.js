3ba474db0d4b4f2b1bb9c0f7c413428268601259
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-25 09:27:14 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -5,7 +5,7 @@ if (!args[0]) throw 'Uhm..url nya mana?'
 let res = await fetch(`https://api.lolhuman.xyz/api/tiktokmusic?apikey=azzbot&url=${args[0]}`)
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendButtonVid(m.chat, json.result.link_dl1, 'Nih Om', watermark, 'Thanks', `Thanks`, m)
+    await conn.sendButtonVid(m.chat, json.result.link, 'Nih Om', watermark, 'Thanks', `Thanks`, m)
 }
 handler.command = /^tiktok$/i
 handler.tags = ['downloader']
