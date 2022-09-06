0ded5565ca593a3bfed48697fb2dac286e60d9db
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-09-06 20:39:34 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
-let res = await fetch(`https://api.xteam.xyz/dl/tiktok?url=${args[0]}&apikey= uNdFM7fR`)
+let res = await fetch(`https://api.xteam.xyz/dl/tiktok?url=${args[0]}&apikey= NezukoTachibana281207`)
     if (!res.ok) throw await res.text()
 let json = await res.json()
     await conn.sendButtonVid(m.chat, json.result.link_dl1, 'Nih Om', watermark, 'Thanks', `Thanks`, m)
